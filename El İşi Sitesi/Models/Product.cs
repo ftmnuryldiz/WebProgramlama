4d7a24117907fcf83d2b1a649d46800f69b14573
ftmnuryldiz <72408171+ftmnuryldiz@users.noreply.github.com>
2021-12-14 22:43:18 +0300

    Update Product.cs
    ViewBag'le tek tek view'e taşımak yerine hepsinin bulunduğu bir model tasarlamak kod okunabilirliğini arttırıyor.

--- El İşi Sitesi/Models/Product.cs
+++ El İşi Sitesi/Models/Product.cs
@@ -1,1 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using projem.entity;
 
+
+
+namespace projem.Models
+{
+    public class Product
+    {
+        //El işi ürünün id, adı, açıklaması, fiyatı bulunabilir. 
+
+        public int ProductId { get; set; }
+
+        //Adı olması zorunludur.
+        [Required(ErrorMessage="Name zorunlu bir alan.")]
+        public string Name { get; set; }
+
+        public double? Price { get; set; }
+
+        [Required(ErrorMessage="Description zorunlu bir alan.")]
+        [StringLength(100,MinimumLength=5,ErrorMessage="Description 5-100 karakter aralığında olmalıdır.")]
+      
+        public string Description { get; set; }    
+        public string ImageUrl { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsHome { get; set; }
+        public List<Category> SelectedCategories { get; set; }
+
+    }
+}
