033fe1c929651ceb8fcc7417037dd41ba617f5a0
ftmnuryldiz <72408171+ftmnuryldiz@users.noreply.github.com>
2021-12-15 17:56:03 +0300

    Update Product.cs

--- El İşi Sitesi/Models/Product.cs
+++ El İşi Sitesi/Models/Product.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ElİsiSitesi.Models
+namespace odevim.Models
 {
     //Ürünümüzün ad, fiyat, id, açıklama, kategori id'si gibi özellikleri olur. 
     public class Product
