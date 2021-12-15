656a7b34369df41490333be36fd634c5a203133d
ftmnuryldiz <72408171+ftmnuryldiz@users.noreply.github.com>
2021-12-15 17:54:09 +0300

    Update Category.cs

--- El İşi Sitesi/Models/Category.cs
+++ El İşi Sitesi/Models/Category.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ElİsiSitesi.Models
+namespace odevim.Models
 {
     public class Category
     {
